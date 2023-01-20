daa7f2addf5675b80131a3c9c48434fd53a59128
adrian.mol <a.moldovanu@wellcomm.gr>
2023-01-20 22:23:24 +0200

    add new table categories

---
+++ migrations/Version20230117184817.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230117184817 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE manufacturer CHANGE date_added date_added DATETIME NOT NULL, CHANGE date_modified date_modified DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE manufacturer CHANGE date_added date_added DATE NOT NULL, CHANGE date_modified date_modified DATE NOT NULL');
+    }
+}
