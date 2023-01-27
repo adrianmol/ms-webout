14a8852f252720798d5663b2fdf41f24b59421fe
adrian.mol <a.moldovanu@wellcomm.gr>
2023-01-28 01:52:03 +0200

    add prisma enum

---
+++ src/Enum/Opencart.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enum;
+
+class Opencart {
+
+    //Credentials
+    public static string $TOKEN_API = 'xklTaedWJORzhqs7';
+
+    public static string $URL = 'https://xikis.webdevs.gr/index.php?route=extension/feed/prisma_win';
+
+    public static string $GET_MANUFACTURER  = 'prismaManufacturers';
+    public static string $GET_CATEGORIES  = 'prismaCategories';
+
+}
