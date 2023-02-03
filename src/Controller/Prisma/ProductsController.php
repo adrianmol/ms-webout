dd16f94090f00abfc32a31bd4906f065d036aab8
adrian.mol <a.moldovanu@wellcomm.gr>
2023-02-04 00:51:35 +0200

    insert or update product from prisma

--- src/Controller/Prisma/ProductsController.php
+++ src/Controller/Prisma/ProductsController.php
@@ -3,7 +3,8 @@
 namespace App\Controller\Prisma;
 
 use Enum\Prisma;
-use App\Entity\Product;
+use App\Entity\Products;
+use App\Entity\ProductDescription;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class ProductsController extends AbstractController
 {
-
 
     private $client;
 
@@ -32,24 +32,76 @@ class ProductsController extends AbstractController
 
         foreach($products['StoreDetails'] as $prisma_product){
 
-            $product = new \App\Entity\Products;
-            //dd($prisma_product);
-            $product->setProductID((string)$prisma_product['ItemId'] ?? 0)
-                    ->setModel((string)$prisma_product['ItemCode'] ?? '')
-                    ->setSku(!empty($prisma_product['ItemBarcode']) ? (string)$prisma_product['ItemBarcode'] :  '')
-                    ->setQuantity((int)$prisma_product['ItemStock'] ?? 0)
-                    ->setManufacturerId((int)$prisma_product['ItemManufacturerId'] ?? '')
-                    ->setWholesalePrice(round($prisma_product['ItemWholesale'], 4) ?? '')
-                    ->setPrice(round($prisma_product['ItemRetail'], 4) ?? 0)
-                    ->setPriceWithVat(round($prisma_product['ItemRetailVat'], 4) ?? 0)
-                    ->setVatPerc(round($prisma_product['ItemFPA'], 2) ?? 0)
-                    ->setWeight((float)$prisma_product['ItemWeight'] ?? 0)
-                    ->setStatus(1)
-                    ->setDateAdded(new \DateTime())
-                    ->setDateModified(new \DateTime());
+            $product_id  = $prisma_product['ItemId'] ?? 0;
+            $category_id = $prisma_product['ItemGroupId'] ?? NULL;
 
-            $entityManager->persist($product);
-            //dd($product);
+            //Init objects <product/productDescription/productCategory>
+            //Search for product if exist
+            $product = new \App\Entity\Products;
+            $productDescription = new \App\Entity\ProductDescription;
+
+            $exist_product = $doctrine->getRepository(\App\Entity\Products::class)->findOneBy(['product_id' => $product_id]);
+            $exist_category_id = $doctrine->getRepository(\App\Entity\Categories::class)->findOneBy(['category_id' => $category_id]);
+
+            $model           = $prisma_product['ItemCode'] ?? '';
+            $sku             = !empty($prisma_product['ItemBarcode']) ? (string)$prisma_product['ItemBarcode'] : '';
+            $quantity        = (int)$prisma_product['ItemStock'] ?? 0;
+            $manufacturer_id = (int)$prisma_product['ItemManufacturerId'] ?? 0;
+            $wholesale_price = round($prisma_product['ItemWholesale'], 4) ?? 0.0000;
+            $price           = round($prisma_product['ItemRetail'], 4) ?? 0.0000;
+            $price_with_vat  = round($prisma_product['ItemRetailVat'], 4) ?? 0.000;
+            $vat_perc        = round($prisma_product['ItemFPA'], 2) ?? 0.00;
+            $weight          = (float)$prisma_product['ItemWeight'] ?? 0.000;
+            
+
+            $name        = $prisma_product['ItemDescr'] ?? '';
+            $description = !empty($prisma_product['ItemNotes']) ? $prisma_product['ItemNotes'] : '';
+
+            if(!$exist_product){
+
+                $productDescription->setName($name)
+                                   ->setDescription($description)
+                                   ->setLanguageId(1);
+
+                $product->setProductID($product_id)
+                        ->setModel($model)
+                        ->setSku($sku)
+                        ->setQuantity($quantity)
+                        ->setManufacturerId($manufacturer_id)
+                        ->setWholesalePrice($wholesale_price)
+                        ->setPrice($price)
+                        ->setPriceWithVat($price_with_vat)
+                        ->setVatPerc($vat_perc)
+                        ->setWeight($weight)
+                        ->setStatus(1)
+                        ->setDateAdded(new \DateTime())
+                        ->setDateModified(new \DateTime())
+                        ->addProductDescription($productDescription);
+
+                if($exist_category_id) $product->addCategory($exist_category_id);
+                
+                $entityManager->persist($product);
+                $entityManager->persist($productDescription);
+                
+            }else{
+
+                $exist_product->setModel($model)
+                              ->setSku($sku)
+                              ->setQuantity($quantity)
+                              ->setManufacturerId($manufacturer_id)
+                              ->setWholesalePrice($wholesale_price)
+                              ->setPrice($price)
+                              ->setPriceWithVat($price_with_vat)
+                              ->setVatPerc($vat_perc)
+                              ->setWeight($weight)
+                              ->setDateModified(new \DateTime())
+                              ->getProductDescriptions()
+                              ->get(0)
+                              ->setName($name)
+                              ->setDescription($description);
+
+                if($exist_category_id) $exist_product->addCategory($exist_category_id);              
+            }
         }
         
         $entityManager->flush();
