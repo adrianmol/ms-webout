0bb01b507a7c456e9f395229496a1cf8d17c4b98
adrian.mol <a.moldovanu@wellcomm.gr>
2023-02-17 23:03:24 +0200

    get product variants from prisma and no eshop products

--- src/Controller/Prisma/ProductsController.php
+++ src/Controller/Prisma/ProductsController.php
@@ -16,21 +16,32 @@ class ProductsController extends AbstractController
 {
 
     private $client;
+    private $doctrine;
 
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
+    public function __construct(
+        HttpClientInterface $client,
+        ManagerRegistry $doctrine
+    ) {
+        $this->client   = $client;
+        $this->doctrine = $doctrine;
     }
 
     #[Route('/prisma/products', name: 'app_prisma_product')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(): Response
     {
 
-        $entityManager = $doctrine->getManager();
+        $entityManager = $this->doctrine->getManager();
 
         $products = $this->getProducts();
 
-        foreach($products['StoreDetails'] as $prisma_product){
+        foreach ($products['StoreDetails'] as $prisma_product) {
+
+            if (
+                isset($prisma_product['ItemIdMaster'])
+                && !empty($prisma_product['ItemIdMaster'])
+            ) {
+                continue;
+            }
 
             $product_id  = $prisma_product['ItemId'] ?? 0;
             $category_id = $prisma_product['ItemGroupId'] ?? NULL;
@@ -40,8 +51,8 @@ class ProductsController extends AbstractController
             $product = new \App\Entity\Products;
             $productDescription = new \App\Entity\ProductDescription;
 
-            $exist_product = $doctrine->getRepository(\App\Entity\Products::class)->findOneBy(['product_id' => $product_id]);
-            $exist_category_id = $doctrine->getRepository(\App\Entity\Categories::class)->findOneBy(['category_id' => $category_id]);
+            $exist_product = $this->doctrine->getRepository(\App\Entity\Products::class)->findOneBy(['product_id' => $product_id]);
+            $exist_category_id = $this->doctrine->getRepository(\App\Entity\Categories::class)->findOneBy(['category_id' => $category_id]);
 
             $model           = $prisma_product['ItemCode'] ?? '';
             $sku             = !empty($prisma_product['ItemBarcode']) ? (string)$prisma_product['ItemBarcode'] : '';
@@ -52,58 +63,56 @@ class ProductsController extends AbstractController
             $price_with_vat  = round($prisma_product['ItemRetailVat'], 4) ?? 0.000;
             $vat_perc        = round($prisma_product['ItemFPA'], 2) ?? 0.00;
             $weight          = (float)$prisma_product['ItemWeight'] ?? 0.000;
-            
 
             $name        = $prisma_product['ItemDescr'] ?? '';
             $description = !empty($prisma_product['ItemNotes']) ? $prisma_product['ItemNotes'] : '';
 
-            if(!$exist_product){
+            if (!$exist_product) {
 
                 $productDescription->setName($name)
-                                   ->setDescription($description)
-                                   ->setLanguageId(1);
+                    ->setDescription($description)
+                    ->setLanguageId(2);
 
                 $product->setProductID($product_id)
-                        ->setModel($model)
-                        ->setSku($sku)
-                        ->setQuantity($quantity)
-                        ->setManufacturerId($manufacturer_id)
-                        ->setWholesalePrice($wholesale_price)
-                        ->setPrice($price)
-                        ->setPriceWithVat($price_with_vat)
-                        ->setVatPerc($vat_perc)
-                        ->setWeight($weight)
-                        ->setStatus(1)
-                        ->setDateAdded(new \DateTime())
-                        ->setDateModified(new \DateTime())
-                        ->addProductDescription($productDescription);
+                    ->setModel($model)
+                    ->setSku($sku)
+                    ->setQuantity($quantity)
+                    ->setManufacturerId($manufacturer_id)
+                    ->setWholesalePrice($wholesale_price)
+                    ->setPrice($price)
+                    ->setPriceWithVat($price_with_vat)
+                    ->setVatPerc($vat_perc)
+                    ->setWeight($weight)
+                    ->setStatus(1)
+                    ->setDateAdded(new \DateTime())
+                    ->setDateModified(new \DateTime())
+                    ->addProductDescription($productDescription);
 
-                if($exist_category_id) $product->addCategory($exist_category_id);
-                
+                if ($exist_category_id) $product->addCategory($exist_category_id);
+
                 $entityManager->persist($product);
                 $entityManager->persist($productDescription);
-                
-            }else{
+            } else {
 
                 $exist_product->setModel($model)
-                              ->setSku($sku)
-                              ->setQuantity($quantity)
-                              ->setManufacturerId($manufacturer_id)
-                              ->setWholesalePrice($wholesale_price)
-                              ->setPrice($price)
-                              ->setPriceWithVat($price_with_vat)
-                              ->setVatPerc($vat_perc)
-                              ->setWeight($weight)
-                              ->setDateModified(new \DateTime())
-                              ->getProductDescriptions()
-                              ->get(0)
-                              ->setName($name)
-                              ->setDescription($description);
+                    ->setSku($sku)
+                    ->setQuantity($quantity)
+                    ->setManufacturerId($manufacturer_id)
+                    ->setWholesalePrice($wholesale_price)
+                    ->setPrice($price)
+                    ->setPriceWithVat($price_with_vat)
+                    ->setVatPerc($vat_perc)
+                    ->setWeight($weight)
+                    ->setDateModified(new \DateTime())
+                    ->getProductDescriptions()
+                    ->get(0)
+                    ->setName($name)
+                    ->setDescription($description);
 
-                if($exist_category_id) $exist_product->addCategory($exist_category_id);              
+                if ($exist_category_id) $exist_product->addCategory($exist_category_id);
             }
         }
-        
+
         $entityManager->flush();
 
         return $this->render('prisma/product/index.html.twig', [
@@ -111,24 +120,162 @@ class ProductsController extends AbstractController
         ]);
     }
 
-    public function getProducts(){
-        // $response = $this->client->request('POST', Prisma::$URL .'/'. Prisma::$GET_PRODUCTS, [
-        //     'body' => ['SiteKey'    => Prisma::$SITE_KEY,
-        //                'Date'       => '10-1-2022',
-        //                'StorageCode'=> '000']
-        // ]); 
+    #[Route('/prisma/products/disabled', name: 'app_prisma_product_disabled')]
+    public function insertDisabledProducts()
+    {
+        $entityManager = $this->doctrine->getManager();
+        $products = $this->getDisabledProducts();
 
-        //return json_decode(json_encode((array)simplexml_load_string($response->getContent())),true);
+        if(!isset($products['StoreItemsNoEshop']) && empty($products['StoreItemsNoEshop']))
+        {
+            return false;
+        }
 
-        //$this->getParameter('kernel.project_dir').'/public/xml/'
-        return json_decode(
-            json_encode(
-                (array)simplexml_load_string(
-                    file_get_contents(
-                        $this->getParameter('kernel.project_dir').'/public/xml/'.'GetProducts.xml'
-                        )
-                    )
-                )
-                ,true);
+        foreach ($products['StoreItemsNoEshop'] as $prisma_product) {
+
+            $model = (string)$prisma_product['storecode'];
+
+            $exist_product = $this->doctrine->getRepository(\App\Entity\Products::class)->findOneBy(['model' => $model]);
+
+            if($exist_product)
+            {
+                $exist_product
+                 ->setStatus(0)
+                 ->setDateModified(new \DateTime())
+                ;
+            }
+        }
+
+        $entityManager->flush();
+
+        return $this->render('prisma/product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/prisma/products/variations', name: 'app_prisma_product_variation')]
+    public function insertProductVariation()
+    {
+        $entityManager = $this->doctrine->getManager();
+
+        $products = $this->getProducts();
+
+        foreach ($products['StoreDetails'] as $prisma_product) {
+
+            if (
+                isset($prisma_product['ItemIdMaster'])
+                && empty($prisma_product['ItemIdMaster'])
+            ) {
+                continue;
+            }
+
+            $variation_id  = $prisma_product['ItemId'] ?? 0;
+
+            $product_variation = new \App\Entity\ProductVariations;
+            $exist_product_variation = $this->doctrine->getRepository(\App\Entity\ProductVariations::class)->findOneBy(['variation_id' => $variation_id]);
+
+            $product_master_id = $prisma_product['ItemIdMaster'] ?? 0;
+            $model           = $prisma_product['ItemCode'] ?? '';
+            $sku             = !empty($prisma_product['ItemBarcode']) ? (string)$prisma_product['ItemBarcode'] : '';
+            $quantity        = (int)$prisma_product['ItemStock'] ?? 0;
+            $price_with_vat  = round($prisma_product['ItemRetailVat'], 4) ?? 0.000;
+            $weight          = (float)$prisma_product['ItemWeight'] ?? 0.000;
+
+            if (empty($prisma_product['Size'])) continue;
+
+            $option_name = (string)$prisma_product['Size'] ?? NULL;
+            $option_value_description = $this->doctrine->getRepository(\App\Entity\OptionValueDescription::class)->findOneBy(['name' => $option_name]);
+
+            if (empty($option_value_description)) {
+                $option_value_description = $this->createOptionValueAndDescription($option_name);
+            }
+
+            if (!$exist_product_variation) {
+                $product_variation->setProductMasterId($product_master_id)
+                    ->setVariationId($variation_id)
+                    ->setOptionId($option_value_description->getOptionId())
+                    ->setOptionValueId($option_value_description->getId())
+                    ->setBarcode($model)
+                    ->setPrice($price_with_vat)
+                    ->setQuantity($quantity);
+
+                $entityManager->persist($product_variation);
+            } else {
+                $exist_product_variation->setProductMasterId($product_master_id)
+                    ->setVariationId($variation_id)
+                    ->setOptionId($option_value_description->getOptionId())
+                    ->setOptionValueId($option_value_description->getId())
+                    ->setBarcode($model)
+                    ->setPrice($price_with_vat)
+                    ->setQuantity($quantity);
+            }
+        }
+
+        $entityManager->flush();
+
+        return $this->render('prisma/product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    private function createOptionValueAndDescription($name)
+    {
+        $option = new \App\Entity\OptionValue;
+        $option_value = new \App\Entity\OptionValueDescription;
+        $optionValueRepository = new \App\Repository\OptionValueRepository($this->doctrine);
+        $optionValueDescriptionRepository = new \App\Repository\OptionValueDescriptionRepository($this->doctrine);
+
+        $optionValueRepository->save(
+            $option->setOptionId(1)->setSortOrder(0),
+            true
+        );
+
+        $optionValueDescriptionRepository->save(
+            $option_value->setId($option->getId())
+                ->setLanguageId(2)
+                ->setOptionId(1)
+                ->setName($name),
+            true
+        );
+
+        return $option_value;
+    }
+
+    private function getDisabledProducts()
+    {
+        $response = $this->client->request('POST', Prisma::$URL . '/' . Prisma::$GET_DISABLED_PRODUCTS, [
+            'body' => [
+                'SiteKey'    => Prisma::$SITE_KEY,
+                'Date'       => '10-1-2022',
+                'StorageCode' => '000'
+            ]
+        ]);
+
+        return json_decode(json_encode((array)simplexml_load_string($response->getContent())), true);
+    }
+
+    private function getProducts()
+    {
+        $response = $this->client->request('POST', Prisma::$URL . '/' . Prisma::$GET_PRODUCTS, [
+            'body' => [
+                'SiteKey'    => Prisma::$SITE_KEY,
+                'Date'       => '10-1-2022',
+                'StorageCode' => '000'
+            ]
+        ]);
+
+        return json_decode(json_encode((array)simplexml_load_string($response->getContent())), true);
+
+        // $this->getParameter('kernel.project_dir').'/public/xml/'
+        // return json_decode(
+        //     json_encode(
+        //         (array)simplexml_load_string(
+        //             file_get_contents(
+        //                 $this->getParameter('kernel.project_dir') . '/public/xml/' . 'GetProducts.xml'
+        //             )
+        //         )
+        //     ),
+        //     true
+        // );
     }
 }
