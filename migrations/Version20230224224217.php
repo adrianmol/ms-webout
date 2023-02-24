088202f4dd93685156b1288841fba2e3a8494aed
adrian.mol <a.moldovanu@wellcomm.gr>
2023-02-25 01:00:11 +0200

    delete migrations files

--- migrations/Version20230224224217.php
+++ migrations/Version20230224224217.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230210160048 extends AbstractMigration
+final class Version20230224224217 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,16 @@ final class Version20230210160048 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE option_value_description MODIFY option_value_id INT NOT NULL');
-        $this->addSql('DROP INDEX `primary` ON option_value_description');
-        $this->addSql('ALTER TABLE option_value_description CHANGE option_value_id option_value_id INT NOT NULL');
-        $this->addSql('ALTER TABLE option_value_description ADD PRIMARY KEY (option_value_id, language_id)');
+        $this->addSql('ALTER TABLE product_discount CHANGE discount_id discount_id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (discount_id)');
         $this->addSql('ALTER TABLE products CHANGE weight weight NUMERIC(15, 4) DEFAULT \'0\' NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX `PRIMARY` ON option_value_description');
-        $this->addSql('ALTER TABLE option_value_description CHANGE option_value_id option_value_id INT AUTO_INCREMENT NOT NULL');
-        $this->addSql('ALTER TABLE option_value_description ADD PRIMARY KEY (option_value_id)');
         $this->addSql('ALTER TABLE products CHANGE weight weight NUMERIC(15, 4) DEFAULT \'0.0000\' NOT NULL');
+        $this->addSql('ALTER TABLE product_discount MODIFY discount_id INT NOT NULL');
+        $this->addSql('DROP INDEX `primary` ON product_discount');
+        $this->addSql('ALTER TABLE product_discount CHANGE discount_id discount_id INT NOT NULL');
     }
 }
