c43da41af40e2062e10b859592844cbe39caf763
adrian.mol <a.moldovanu@wellcomm.gr>
2023-03-05 21:49:36 +0200

    if the user isnt login redirect to home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
