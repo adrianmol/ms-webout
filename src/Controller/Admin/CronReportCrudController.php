6f2c3b17075fa7e4c99e315e1871dbff6e741513
adrian.mol <a.moldovanu@wellcomm.gr>
2023-03-11 01:28:59 +0200

    cron report dashboard

--- src/Controller/Admin/CronReportCrudController.php
+++ src/Controller/Admin/CronReportCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use Cron\CronBundle\Entity\CronReport;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class CronReportCrudController extends AbstractCrudController
 {
@@ -12,14 +20,17 @@ class CronReportCrudController extends AbstractCrudController
         return CronReport::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            //TextareaField::new('getJobName'),
+            DateTimeField::new('run_at'),
+            NumberField::new('run_time'),
+            NumberField::new('exit_code'),
+            TextEditorField::new('output'),
+            TextareaField::new('error'),
         ];
     }
-    */
+
 }
